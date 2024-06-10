8c44fd85a8ffbd918865d468e9f54182437b300f
entbappy <entbappy73@gmail.com>
2024-06-10 20:59:04 +0600

    updated

---
+++ main.py
@@ -0,0 +1,85 @@
+import boto3
+import streamlit as st
+from langchain.llms.bedrock import Bedrock
+from langchain.embeddings import BedrockEmbeddings
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+
+
+prompt_template = """
+
+Human: Use the following pieces of context to provide a 
+concise answer to the question at the end but use atleast summarize with 
+250 words with detailed explantions. If you don't know the answer, 
+just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context
+
+Question: {question}
+
+Assistant:"""
+
+
+#Bedrock client
+bedrock = boto3.client(service_name = "bedrock-runtime", region_name = "us-east-1")
+
+#Get embeddings model from bedrock
+bedrock_embedding = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client= bedrock)
+
+
+
+def get_documents():
+    loader = PyPDFDirectoryLoader("Data")
+    documents = loader.load()
+    text_spliter = RecursiveCharacterTextSplitter(
+                                        chunk_size=1000, 
+                                        chunk_overlap=500)
+    docs = text_spliter.split_documents(documents)
+    return docs
+
+
+def get_vector_store(docs):
+   vectorstore_faiss =  FAISS.from_documents(
+        docs,
+        bedrock_embedding
+    )
+   vectorstore_faiss.save_local("faiss_local")
+
+
+
+def get_llm():
+    llm = Bedrock(model_id = "mistral.mistral-7b-instruct-v0:2", client = bedrock)
+    return llm
+
+
+
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
+def get_llm_response(llm, vectorstore_faiss, query):
+
+    qa = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever= vectorstore_faiss.as_retriever(
+        search_type="similarity", search_kwargs={"k": 3}),
+
+        return_source_documents = True,
+        chain_type_kwargs={"prompt": PROMPT})
+
+    
+    response = qa({"query": query})
+    return response['results']
+
+
+
+def main():
+    pass
